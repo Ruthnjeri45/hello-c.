9ade4f36583c5a3c8695d5a503d7865b793af3b5
Ruthnjeri45 <njeeruth45@gmail.com>
2026-09-16 11:46:17 +0300

    Add files via upload

---
+++ c programming.c
@@ -0,0 +1,21 @@
+/*
+Name:Ruth Njeri
+Reg NO:CT100/G/30659/26
+*/
+
+#include <stdio.h>
+
+int main(){
+	printf("Units offered this semester.\n");
+	printf("1.Fundamentals Of Digital Technology.\n");
+	printf("2.Introduction Of Computer.\n");
+	printf("3.Essentials Of Accounting.\n");
+	printf("4.Computer Systems.\n");
+	printf("5.Analogue Electronics.\n");
+	printf("6.Mathematics For Science.\n");
+	printf("7.Introduction To Programming.\n");
+	printf("8.Communication And Information Literacy Skills.\n");
+	
+		return 0;//execution successful
+}
+	
