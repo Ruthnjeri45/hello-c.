3ba9e1da863412832ab7964537107a1f295c016a
Ruthnjeri45 <njeeruth45@gmail.com>
2026-09-24 19:39:57 +0300

    Add files via upload

---
+++ c5 programming.cpp
@@ -0,0 +1,26 @@
+/*
+Name:Ruth Njeri
+Reg No:CT100/G/30659/26
+Description:week 1 task 2
+
+*/
+#include<stdio.h>
+
+int main(){
+	//declare  variables
+	float height =156;//%f
+	double  balance=50000;//%lf
+	long double number=0745606005;//%lf
+	
+	printf("enter your height in cm;\t");
+	scanf("%f",&height );
+	
+	printf("enteryour bank balance;\t");
+	scanf("%lf", &balance);
+	
+	printf("enter your phone number;\t");
+	scanf("%lf", &number);
+	
+	return 0;	
+	
+}
